dbd301d829aa159d44a037633725d439ac9d2993
Rhea Kapadia <rkapadia@uncc.edu>
2021-11-19 18:31:21 -0500

    set up environment for app

--- runtimeTerror.py
+++ runtimeTerror.py
@@ -7,7 +7,7 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
-from models import Note as Note
+# from models import Post as Post
 from models import User as User
 
 app = Flask(__name__)  # create an app
@@ -29,47 +29,47 @@ def index():
     return render_template('index.html', user=a_user)
 
 
-@app.route('/notes')
-def get_notes():
-    a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu')
-    my_notes = db.session.query(Note).all()
-    return render_template('notes.html', notes=my_notes, user=a_user)
-
-
-@app.route('/notes/<note_id>')
-def get_note(note_id):
-
-    a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('note.html', note=my_note, user=a_user)
-
-
-@app.route('/notes/new', methods=['GET', 'POST'])
-def new_note():
-
-    # check method used for request
-    if request.method == 'POST':
-        # create title data
-        title = request.form['title']
-
-        # get note data
-        text = request.form['noteText']
-
-        # create date stamp
-        from datetime import date
-        today = date.today()
-
-        # format date mm/dd/yyyy
-        today = today.strftime('%m-%d-%Y')
-        new_record = Note(title, text, today)
-        db.session.add(new_record)
-        db.session.commit()
-
-        return redirect(url_for('get_notes'))
-
-    else:
-        a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu').one()
-        return render_template('new.html', user=a_user)
+# @app.route('/notes')
+# def get_notes():
+#     a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu')
+#     my_notes = db.session.query(Post).all()
+#     return render_template('notes.html', notes=my_notes, user=a_user)
+#
+#
+# @app.route('/notes/<note_id>')
+# def get_note(note_id):
+#
+#     a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu').one()
+#     my_note = db.session.query(Post).filter_by(id=note_id).one()
+#     return render_template('note.html', note=my_note, user=a_user)
+#
+#
+# @app.route('/notes/new', methods=['GET', 'POST'])
+# def new_note():
+#
+#     # check method used for request
+#     if request.method == 'POST':
+#         # create title data
+#         title = request.form['title']
+#
+#         # get note data
+#         text = request.form['noteText']
+#
+#         # create date stamp
+#         from datetime import date
+#         today = date.today()
+#
+#         # format date mm/dd/yyyy
+#         today = today.strftime('%m-%d-%Y')
+#         new_record = Post(title, text, today)
+#         db.session.add(new_record)
+#         db.session.commit()
+#
+#         return redirect(url_for('get_notes'))
+#
+#     else:
+#         a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu').one()
+#         return render_template('new.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
