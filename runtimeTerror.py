671ed263c685d8125dc96d7d3a6c316890c3533d
Rhea Kapadia <rkapadia@uncc.edu>
2021-11-23 01:38:15 -0500

    created and added functionality for the update_post method

--- runtimeTerror.py
+++ runtimeTerror.py
@@ -81,6 +81,34 @@ def delete_post(post_id):
     return redirect(url_for('get_posts'))
 
 
+@app.route('/posts/edit/<post_id>', methods=['GET', 'POST'])
+def update_post(post_id):
+    # check method used for request
+    if request.method == 'POST':
+        # get title data
+        title = request.form['title']
+        # get post data
+        text = request.form['postText']
+        post = db.session.query(Post).filter_by(id=post_id).one()
+        # update post data
+        post.title = title
+        post.text = text
+        # update post in DB
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('get_posts'))
+    else:
+        # GET request= show new post form to edit post
+        # Retrieve user from database
+        a_user = db.session.query(User).filter_by(email='rkapadia@uncc.edu').one()
+
+        # retrieve note from database
+        my_post = db.session.query(Post).filter_by(id=post_id).one()
+
+    return render_template('new.html', post=my_post, user=a_user)
+
+
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
